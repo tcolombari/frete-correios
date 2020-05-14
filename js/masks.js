2160b46097e2fa48947dc6367a6e93a3f48b8114
gcairesdev <pessoalgcaires@gmail.com>
2020-05-14 12:56:05 -0300

    :rocket: Adiciona as máscaras para os inputs

---
+++ js/masks.js
@@ -0,0 +1,26 @@
+function cepMask(element) {
+    var input = element;
+    var cep = input.value;
+
+    input.setAttribute('maxlength', 8);
+
+    newCep = cep.replace(/(\D)/g, '').replace(/^(\d{5})(\d{3})/, '$1-$2');
+    input.value = newCep;
+}
+
+function decimalMask(element) {
+    var input = element;
+    var price = input.value;
+
+    input.setAttribute('maxlength', 10)
+    
+    var newPrice = 
+        (price.replace(/(\D)/g, '') / 100)
+        .toFixed(2)
+        .replace('.', ',')
+        .replace(/(\d)(\d{3})(\d{3}),/g, '$1.$2.$3')
+        .replace(/(\d)(\d{3}),/g, '$1.$2,');
+
+    input.value = newPrice;
+}
+
