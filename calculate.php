ac1d0bad88cf994236d5c541dfbaa27fdfecaed6
gcairesdev <pessoalgcaires@gmail.com>
2020-05-19 22:09:33 -0300

    :rockeat: Add paginação e resultado via ajax

--- calculate.php
+++ calculate.php
@@ -1,10 +1,10 @@
 <?php
-
     extract($_REQUEST);
 
     $zipCode = str_replace("-","",$zipCode);
     $zipCodeDest = str_replace("-","",$zipCodeDest);
     $weight = str_replace(",",".",str_replace(".","",$weight));
+    $width = str_replace(",",".",str_replace(".","",$width));
     $lenght = str_replace(",",".",str_replace(".","",$lenght));
     $height = str_replace(",",".",str_replace(".","",$height));
     $price = str_replace(",",".",str_replace(".","",$price));
@@ -23,7 +23,7 @@ function getFreightInfo($zipCode, $zipCodeDest, $weight, $width, $height, $lengh
     $url .= "&nVlAltura=" . $height;
     $url .= "&nCdFormato=1";
     $url .= "&nVlComprimento=" . $lenght;
-    $url .= "&sCdMaoProria=n";
+    $url .= "&sCdMaoPropria=n";
     $url .= "&nVlValorDeclarado=" . $price;
     $url .= "&sCdAvisoRecebimento=n";
     $url .= "&nCdServico=" . $sendType;
@@ -32,44 +32,22 @@ function getFreightInfo($zipCode, $zipCodeDest, $weight, $width, $height, $lengh
     $url .= "&nIndicaCalculo=3";
 
     $xml = simplexml_load_file($url);
+    $sendType = $xml->cServico->Codigo == 40010 ? 'SEDEX' : 'PAC';
 
     if($xml->cServico->Erro=="0"){
-        return $xml;
+        $arr = [
+            'error' => false,
+            $sendType,
+            $xml->cServico->Valor
+        ];
+        echo (json_encode ($arr));
     } else{
-        return $xml->cServico->MsgErro;
+        $arr = [
+            'error' => true,
+            $xml->cServico->MsgErro
+        ];
+        echo (json_encode ($arr));
+        echo $xml->cServico->MsgErro;   
     }
 }
-
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Informações de envio</title>
-</head>
-<body> 
-    <h1>Informações de envio</h1>
-    <table>
-        <thead>
-            <tr>
-                <th scope="col">CEP Destino</th>
-                <th scope="col">Serviço</th>
-                <th scope="col">Valor</th>
-                <th scope="col">Prazo de Entrega</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><?= $zipCodeDest; ?></td>
-                <td><?= ($info->cServico->Codigo == "41106") ? "PAC" : "SEDEX" ; ?></td>
-                <td><?= "R$ ".$info->cServico->Valor; ?></td>
-                <td><?= $info->cServico->PrazoEntrega . " dias"; ?></td>
-            </tr>
-        </tbody>
-    </table>
-    <script src="js/masks.js"></script>
-</body>
-</html>
